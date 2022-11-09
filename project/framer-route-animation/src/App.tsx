714f5b35d6a32717d65045f2edb120ec002ae8ff
sillypoise <somewhatjose@gmail.com>
2022-11-09 15:32:12 -0500

    p/framer-route-animation: init project, POC using react router + framer motion

---
+++ project/framer-route-animation/src/App.tsx
@@ -0,0 +1,63 @@
+import { AnimatePresence, motion, useIsPresent } from "framer-motion";
+import React, { useState } from "react";
+import { useLocation, useRoutes } from "react-router-dom";
+
+function Page({ page }: { page: string }) {
+    let isPresent = useIsPresent();
+    return (
+        <main className="mlb-l">
+            <motion.div
+                initial={{ scaleX: 1 }}
+                animate={{
+                    scaleX: 0,
+                    transition: { duration: 0.5, ease: "circOut" },
+                }}
+                exit={{
+                    scaleX: 1,
+                    transition: { duration: 0.5, ease: "circIn" },
+                }}
+                style={{ originX: isPresent ? 0 : 1 }}
+                className="imposter inset-[0] transform-none bg-dark-tomato-9"
+            />
+            <article className="center stack">
+                <div className="cover">
+                    <h1>{page}</h1>
+                </div>
+            </article>
+        </main>
+    );
+}
+
+function App() {
+    let location = useLocation();
+
+    const element = useRoutes([
+        {
+            path: "/",
+            element: <Page page="Home" />,
+        },
+        {
+            path: "/a",
+            element: <Page page="A" />,
+        },
+        {
+            path: "/b",
+            element: <Page page="A" />,
+        },
+    ]);
+
+    if (!element) return null;
+
+    return (
+        <main className="mlb-l">
+            <article className="center stack max-is-[55ch]">
+                <h1 className="text-3">Framer Route Animation</h1>
+                <AnimatePresence mode="wait" initial={false}>
+                    {React.cloneElement(element, { key: location.key })}
+                </AnimatePresence>
+            </article>
+        </main>
+    );
+}
+
+export default App;
