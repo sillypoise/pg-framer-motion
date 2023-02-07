ed9976bc6c10656479d70b96d3df7fb2827dba7e
sillypoise <somewhatjose@gmail.com>
2023-02-07 14:14:05 -0500

    main: starting again

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,3 +1,5 @@
+import { Link } from "@remix-run/react";
+
 export default function Index() {
   return (
     <main className="center mlb-l">
@@ -5,27 +7,10 @@ export default function Index() {
         <h3 className="text-3">Framer Motion Playground</h3>
         <ul className="stack">
           <li>
-            <a
-              target="_blank"
-              href="https://remix.run/tutorials/blog"
-              rel="noreferrer"
-            >
-              15m Quickstart Blog Tutorial
-            </a>
+            <Link to="/">Home</Link>
           </li>
           <li>
-            <a
-              target="_blank"
-              href="https://remix.run/tutorials/jokes"
-              rel="noreferrer"
-            >
-              Deep Dive Jokes App Tutorial
-            </a>
-          </li>
-          <li>
-            <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-              Remix Docs
-            </a>
+            <Link to="/basics">Basic Motion</Link>
           </li>
         </ul>
       </article>
