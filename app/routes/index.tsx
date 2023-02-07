9664ce53420762f634799c39ad6b58b91a02ac43
sillypoise <somewhatjose@gmail.com>
2023-02-07 10:03:45 -0500

    main: migration to remix app

---
+++ app/routes/index.tsx
@@ -0,0 +1,34 @@
+export default function Index() {
+  return (
+    <main className="center mlb-l">
+      <article className="stack">
+        <h3 className="text-3">Framer Motion Playground</h3>
+        <ul className="stack">
+          <li>
+            <a
+              target="_blank"
+              href="https://remix.run/tutorials/blog"
+              rel="noreferrer"
+            >
+              15m Quickstart Blog Tutorial
+            </a>
+          </li>
+          <li>
+            <a
+              target="_blank"
+              href="https://remix.run/tutorials/jokes"
+              rel="noreferrer"
+            >
+              Deep Dive Jokes App Tutorial
+            </a>
+          </li>
+          <li>
+            <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
+              Remix Docs
+            </a>
+          </li>
+        </ul>
+      </article>
+    </main>
+  );
+}
